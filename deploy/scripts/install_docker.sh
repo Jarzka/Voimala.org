9215afe362f20e44d4a1129cd8460e98fbf17bde
Jari Hanhela <jarihanhela@gmail.com>
2016-09-14 20:56:33 +0300

    Use official Docker installation script

--- deploy/scripts/install_docker.sh
+++ deploy/scripts/install_docker.sh
@@ -1,18 +1,13 @@
-wget -qO- https://get.docker.com/ | sh
+# Official installation guidelines: https://docs.docker.com/engine/installation/linux/ubuntulinux/
 
-# Install docker-compose
-COMPOSE_VERSION=`git ls-remote https://github.com/docker/compose | grep refs/tags | grep -oP "[0-9]+\.[0-9]+\.[0-9]+$" | tail -n 1`
-sudo sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
-sudo chmod +x /usr/local/bin/docker-compose
-sudo sh -c "curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
+sudo apt-get update;
+sudo apt-get -y install apt-transport-https ca-certificates;
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D;
+echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" > /etc/apt/sources.list.d/docker.list;
+sudo apt-get update;
+sudo apt-get purge lxc-docker;
+apt-cache policy docker-engine;
+sudo apt-get install -y docker-engine;
+sudo service docker start;
 
-# Install docker-cleanup command
-cd /tmp
-git clone https://gist.github.com/76b450a0c986e576e98b.git
-cd 76b450a0c986e576e98b
-sudo mv docker-cleanup /usr/local/bin/docker-cleanup
-sudo chmod +x /usr/local/bin/docker-cleanup
-
-# To prevent "Docker command can't connect to docker daemon"
-# (it seems that this needs to be executed manually for the first time to work properly?)
-sudo usermod -aG docker $(whoami);
+sudo usermod -aG docker $(whoami); # To be able to run docker with this user (needs manual logout / restart?)
