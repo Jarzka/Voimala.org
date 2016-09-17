eeaf19fc5347db425a6558d2764518c22699fa4a
Jari Hanhela <jarihanhela@gmail.com>
2016-09-17 17:46:18 +0300

    Update home page

--- src/clj/voimala/views/home.clj
+++ src/clj/voimala/views/home.clj
@@ -8,5 +8,9 @@
    [:h1 "Welcome!"]
    [:p "This is a personal website / portfolio of Jari Hanhela, also known as Jarzka."]
    [:p "The main contents of this site are various software development projects I have completed mostly on my free time. I have many years of experience in programming and software development, mostly in web development (both frontend and backend) and game programming."]
-   [:p "This website is powered by "
-    [:a {:href "http://clojure.org"} "Clojure"] "."]])
+   [:p "Data for geeks: this website is powered by "
+    [:a {:href "http://clojure.org"} "Clojure"]
+    " and the deployment is done using "
+    [:a {:href "https://www.ansible.com/"} "Ansible"]
+    " and "
+    [:a {:href "https://www.docker.com/"} "Docker"]]])
