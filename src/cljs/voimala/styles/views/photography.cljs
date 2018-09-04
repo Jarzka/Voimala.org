d33fda506fac313b7eca32fef3fc49615fdc0833
Jari Hanhela <jarihanhela@gmail.com>
2018-09-04 15:46:22 +0300

    Add photos

---
+++ src/cljs/voimala/styles/views/photography.cljs
@@ -0,0 +1,6 @@
+(ns voimala.styles.views.photography
+  (:require [stylefy.core :as stylefy]
+            [garden.color :as color]))
+
+(def photo {:width "100%"
+            :margin-bottom "0.5rem"})
--- src/cljs/voimala/views/photography.cljs
+++ src/cljs/voimala/views/photography.cljs
@@ -1,5 +1,6 @@
 (ns voimala.views.photography
   (:require [stylefy.core :refer [use-style sub-style use-sub-style]]
+            [voimala.styles.views.photography :as pstyle]
             [voimala.ui.general :as ui]))
 
 (defn photography []
@@ -15,4 +16,19 @@
     " or "
     [ui/link {:href "https://www.dropbox.com/sh/spbl6pmblklrmsh/AAAxMrdQsZ-P9ADdSE-odjQka?dl=0"}
      "Dropbox"]
-    "."]])
+    "."]
+
+   [:div {:class "container"}
+    [:div {:class "row"}
+     [:div {:class "col-12 col-md-6 col-lg-4"}
+      [:img (use-style pstyle/photo {:src "images/photos/summer1_thumb.jpg"})]]
+     [:div {:class "col-12 col-md-6 col-lg-4"}
+      [:img (use-style pstyle/photo {:src "images/photos/summer2_thumb.jpg"})]]
+     [:div {:class "col-12 col-md-6 col-lg-4"}
+      [:img (use-style pstyle/photo {:src "images/photos/summer3_thumb.jpg"})]]
+     [:div {:class "col-12 col-md-6 col-lg-4"}
+      [:img (use-style pstyle/photo {:src "images/photos/winter1_thumb.jpg"})]]
+     [:div {:class "col-12 col-md-6 col-lg-4"}
+      [:img (use-style pstyle/photo {:src "images/photos/winter2_thumb.jpg"})]]
+     [:div {:class "col-12 col-md-6 col-lg-4"}
+      [:img (use-style pstyle/photo {:src "images/photos/autumn1_thumb.jpg"})]]]]])
