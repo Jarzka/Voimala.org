fc3a00ee94ae3752ece897f5c45d19bf266f90b7
Jari Hanhela <jarihanhela@gmail.com>
2018-09-08 22:14:30 +0300

    Update welcome section

--- src/cljs/voimala/views/welcome.cljs
+++ src/cljs/voimala/views/welcome.cljs
@@ -7,4 +7,4 @@
    [ui/h1 "Welcome!"]
    [ui/p "This is a personal website / portfolio of Jari Hanhela, also known as Jarzka."]
    [ui/p "The site consists of my projects of various topics (software, photography, writing, games etc). Feel free to explore!"]
-   [ui/p "Contents updated: 4.9.2018."]])
+   [ui/p "Contents updated: 8.9.2018."]])
