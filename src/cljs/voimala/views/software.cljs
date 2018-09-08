0c2a459ad7a89549ebdd4c4776d5ae3f93fd0db0
Jari Hanhela <jarihanhela@gmail.com>
2018-09-08 21:41:42 +0300

    Add video links

--- src/cljs/voimala/views/software.cljs
+++ src/cljs/voimala/views/software.cljs
@@ -37,7 +37,7 @@
      [:span " "]
      "A simple HTML5 marquee library. 4.9.2014."]]])
 
-(defn project-card [{:keys [title date live-demo source-code image-file desc]}]
+(defn project-card [{:keys [title date live-demo video source-code image-file desc]}]
   [:div.card (use-style pstyle/card)
    [:img.card-img-top {:alt "", :src (str "images/" image-file)}]
    [:div.card-body
@@ -45,10 +45,34 @@
     [:p.card-text desc]
     (when live-demo
       [:a.card-link (use-style g-styles/a {:href live-demo}) "Live demo"])
+    (when video
+      [:a.card-link (use-style g-styles/a {:href video}) "Video"])
     (when source-code
-      [:a.card-link (use-style g-styles/a {:href source-code}) "Source code"])
-    ]
+      [:a.card-link (use-style g-styles/a {:href source-code}) "Source code"])]
    [:div.card-footer [:small.text-muted date]]])
+
+(defn- featured []
+  [:div
+   [ui/h2 "Featured"]
+   [:div.card-deck
+        [project-card {:title "Ajokit"
+                       :source-code "https://github.com/Jarzka/Ajokit"
+                       :live-demo "projects/ajokit"
+                       :image-file "ajokit.jpg"
+                       :date "10.10.2015"
+                       :desc "A simple grid-based traffic simulation application which also has a real-time map editing capability."}]
+        [project-card {:title "JarzkaChess"
+                       :source-code "https://github.com/Jarzka/JarzkaChess"
+                       :image-file "jarzkachess.png"
+                       :date "15.2.2014"
+                       :video "https://www.youtube.com/watch?v=viFZeI8uAVU"
+                       :desc "A chess game which includes an artifical intelligence to challenge beginning chess players."}]
+        [project-card {:title "MyRTS"
+                       :source-code "https://github.com/Jarzka/MyRTS"
+                       :image-file "myrts.jpg"
+                       :date "18.4.2014"
+                       :video "https://www.youtube.com/watch?v=OEy6PhbeE_Y"
+                       :desc "MyRTS is a multi player Real Time Strategy game prototype. The architecture supports some basic features like giving simple commands to units and synchronizing the game over network using simultaneous simulations with lockstep-like model. This kind of architectural style has been used in many classic RTS games like Age of Empires."}]]])
 
 (defn software []
   [:div
@@ -57,25 +81,7 @@
     "Our civilization depends critically on software; it had better be quality software."
     "Bjarne Stroustrup"]
 
-   [ui/h2 "Featured"]
-   [:div.card-deck
-    [project-card {:title "Ajokit"
-                   :source-code "https://github.com/Jarzka/Ajokit"
-                   :live-demo "projects/ajokit"
-                   :image-file "ajokit.jpg"
-                   :date "10.10.2015"
-                   :desc "A simple grid-based traffic simulation application which also has a real-time map editing capability."}]
-    [project-card {:title "JarzkaChess"
-                   :source-code "https://github.com/Jarzka/JarzkaChess"
-                   :image-file "jarzkachess.png"
-                   :date "15.2.2014"
-                   :desc "A chess game which includes an artifical intelligence to challenge beginning chess players."}]
-    [project-card {:title "MyRTS"
-                   :source-code "https://github.com/Jarzka/MyRTS"
-                   :image-file "myrts.jpg"
-                   :date "18.4.2014"
-                   :desc "MyRTS is a multi player Real Time Strategy game prototype. The architecture supports some basic features like giving simple commands to units and synchronizing the game over network using simultaneous simulations with lockstep-like model. This kind of architectural style has been used in many classic RTS games like Age of Empires."}]]
-
+   [featured]
    [other-projects]
    [libraries]
 
