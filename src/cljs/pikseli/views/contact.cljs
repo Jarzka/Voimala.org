23e97fc03dfe02782a8332bea90835b4e0c9f990
Jari Hanhela <jarihanhela@gmail.com>
2019-07-24 13:46:41 +0300

    Add one more YouTube link

--- src/cljs/pikseli/views/contact.cljs
+++ src/cljs/pikseli/views/contact.cljs
@@ -15,4 +15,5 @@
    [ui/social-media-link {:some-type :linkedin :href "http://lnkd.in/5JP3nQ"} "LinkedIn"]
    [ui/social-media-link {:some-type :twitter :href "https://twitter.com/jarzka"} "Twitter"]
    [ui/social-media-link {:some-type :youtube :href "https://www.youtube.com/channel/UC1xtM_UXTnSuqmvoBem-cFA"} "YouTube (JarzkaCodes)"]
-   [ui/social-media-link {:some-type :youtube :href "https://www.youtube.com/channel/UCLY7kXdrpY_9RONQPN0J71w"} "YouTube (JarzkaPlays)"]])
+   [ui/social-media-link {:some-type :youtube :href "https://www.youtube.com/channel/UCLY7kXdrpY_9RONQPN0J71w"} "YouTube (JarzkaPlays)"]
+   [ui/social-media-link {:some-type :youtube :href "https://www.youtube.com/channel/UCK7NY-ApqX23M_bQneEgFcQ"} "YouTube (JarzkaFilms)"]])
