5abe59bc28e7a2f5555c7b4f2530aa5c620c6fdf
Jari Hanhela <jarihanhela@gmail.com>
2019-11-28 20:23:58 +0200

    Update shadow-cljs settings

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
+# Description
+
 The source code of my personal website. Does not include assets (images, projects etc.)
+
+# Build
+
+## Develop
+
+```bash
+npx shadow-cljs watch frontend
+```
+
+## Compile production
+
+```bash
+npx shadow-cljs release frontend
+```
--- project.clj
+++ project.clj
@@ -1,18 +1,14 @@
 (defproject Pikseli "1.0.0-SNAPSHOT"
   :description "Pikseli.org"
   :url "https://www.pikseli.org"
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [org.clojure/clojurescript "1.10.439"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/clojurescript "1.10.520"]
                  [reagent "0.8.1"]
-                 [ring "1.6.1"]
                  [http-kit "2.2.0"]
                  [hiccup "1.0.5"]
-                 [clj-time "0.13.0"]
                  [stylefy "1.14.0"]
                  [garden "1.3.2"]
-                 [com.andrewmcveigh/cljs-time "0.5.0"]
-                 [com.taoensso/timbre "4.10.0"]
-                 [compojure "1.6.0"]]
+                 [com.andrewmcveigh/cljs-time "0.5.0"]]
   :main pikseli.core
   :aot :all
   :auto-clean false ; For uberjar
