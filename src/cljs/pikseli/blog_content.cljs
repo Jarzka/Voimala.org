557854efc48b7e97e380d38409c2f324f0207196
Jari Hanhela <jarihanhela@gmail.com>
2020-05-02 21:35:54 +0300

    Blog stuff...

--- src/cljs/pikseli/blog_content.cljs
+++ src/cljs/pikseli/blog_content.cljs
@@ -13,12 +13,17 @@
             [pikseli.styles.layout :as layout]
             [pikseli.services.router :as router]))
 
-(defn get-posts [files ok error]
-  (doseq [file files]
-    (ajax/GET!
-      (str "blog/" file ".md")
-      {:ok ok
-       :error error})))
+(def loaded-posts (r/atom {}))
+
+(defn get-post [file-name ok error]
+  (ajax/GET!
+    (str "blog/" file-name ".md")
+    {:ok #(ok file-name %)
+     :error error}))
+
+(defn get-posts [file-names ok error]
+  (doseq [file-name file-names]
+    (get-post file-name ok error)))
 
 (defn get-post-files [ok error]
   (ajax/GET!
@@ -29,8 +34,28 @@
              (ok lines-stored)))
      :error error}))
 
+(defn single-blog-post [id]
+  #_(r/create-class
+    {:component-did-mount (fn []
+                            (doseq [post-index (range 0 (count parsed-posts))]
+                              (let [post (get parsed-posts post-index)
+                                    content (:content post)
+                                    element (.getElementById js/document (str "postaus-" post-index))
+                                    parsed (marked content)]
+                                #_(println "Parsed: " parsed)
+                                ; TODO Include metadata and title in HTML page
+                                (set! (.. element -innerHTML) parsed))))
+     :render
+     (fn []
+       [:div
+        [:article
+         ; TODO Link to single post
+         [:h1 (:title metadata)]
+         [:div (use-style blog-style/blog-post
+                          {:id (str "postaus-" index)})]]])}))
+
 (defn post-list [posts]
-  (let [parsed-posts (mapv #(js->clj (metadata-parser %) :keywordize-keys true) posts)]
+  (let [parsed-posts (mapv #(js->clj (metadata-parser %) :keywordize-keys true) (vals posts))]
     (r/create-class
       {:component-did-mount (fn []
                               (doseq [post-index (range 0 (count parsed-posts))]
@@ -54,28 +79,27 @@
                                 {:id (str "postaus-" index)})]])
             parsed-posts)])})))
 
-(defn content []
-  (let [post-files (r/atom nil)
-        posts (r/atom [])
+(defn blog-home []
+  (let [post-file-names (r/atom nil)
         error? false
-        handle-error #(reset! error? true)
-        blog-post-id (router/blog-post-id)]
+        handle-error #(reset! error? true)]
 
     (dom/set-title "Kotona ikimetsässä -retkiblogi")
 
     (r/create-class
       {:component-did-mount (fn []
                               (get-post-files
-                                (fn [files]
-                                  (reset! post-files files)
-                                  (get-posts files
-                                             (fn [file]
-                                               (swap! posts conj file))
+                                (fn [file-names]
+                                  (reset! post-file-names file-names)
+                                  (get-posts file-names
+                                             (fn [file-name contents]
+                                               (swap! loaded-posts assoc file-name contents))
                                              handle-error))
                                 handle-error))
        :render
        (fn []
-         (let [all-files-loaded? (= (count @posts) (count @post-files))]
+         (println "LADATTU: " @loaded-posts)
+         (let [all-files-loaded? (= (count (keys @loaded-posts)) (count @post-file-names))]
            [:div
             [:header (use-style blog-style/header)
              [:a (use-style blog-style/back-to-pikseli {:href "http://www.pikseli.org"})
@@ -83,6 +107,12 @@
              [:img (use-sub-style layout/site-header :logo-blog
                                   {:alt "Kotona ikimetsässä" :src "images/logo_blog.png"})]]
             (cond
-              (empty? @posts) [loader/loader {:text "Odota hetki..."}]
               error? "Virhe"
-              all-files-loaded? [post-list @posts])]))})))
+              all-files-loaded? [post-list @loaded-posts]
+              :default [loader/loader {:text "Odota hetki..."}])]))})))
+
+(defn content []
+  (let [blog-post-id (router/blog-post-id)]
+    (if blog-post-id
+      [single-blog-post blog-post-id]
+      [blog-home])))
