202e0103d0cbc05e10f0aeef20a14969c4b290e9
Jari Hanhela <jarihanhela@gmail.com>
2020-09-06 11:39:02 +0300

    Remove tags correctly

--- src/cljs/pikseli/common/services/dom.cljs
+++ src/cljs/pikseli/common/services/dom.cljs
@@ -23,9 +23,8 @@
            meta[property='og:title'],
            meta[property='og:description'],
            meta[property='og:image']")]
-    (when (coll? meta-tags)
-      (doseq [element meta-tags]
-       (.remove element)))))
+    (doseq [element meta-tags]
+      (.remove element))))
 
 
 (defn set-meta-tags [{:keys [title type author uri image description keywords]}]
