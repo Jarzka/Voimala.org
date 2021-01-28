a5dcbb0016df35ec53408eed1ac583527e80450b
Jari Hanhela <jarihanhela@gmail.com>
2021-01-28 18:21:44 +0200

    Refer reg-event-db

--- src/cljs/pikseli/blog/services/blog.cljs
+++ src/cljs/pikseli/blog/services/blog.cljs
@@ -1,6 +1,6 @@
 (ns pikseli.blog.services.blog
   (:require [reagent.core :as r]
-            [re-frame.core :as re-frame]))
+            [re-frame.core :refer [reg-event-db]]))
 
 (def state
   {:current-page-index 0
@@ -9,43 +9,43 @@
    :posts-loading      #{} ; A set of post ids being loaded
    :error?             false})
 
-(re-frame/reg-event-db
+(reg-event-db
   ::init
   (fn [db [_]]
     state))
 
-(re-frame/reg-event-db
+(reg-event-db
   ::reset-post-ids
   (fn [db [_ post-ids]]
     (assoc db :post-ids post-ids)))
 
-(re-frame/reg-event-db
+(reg-event-db
   ::set-error
   (fn [db [_]]
     (assoc db :error? true)))
 
-(re-frame/reg-event-db
+(reg-event-db
   ::set-current-page-index
   (fn [db [_ index]]
     (assoc db :current-page-index index)))
 
-(re-frame/reg-event-db
+(reg-event-db
   ::set-post-contents
   (fn [db [_ post-id contents]]
     (assoc-in db [:posts post-id] contents)))
 
-(re-frame/reg-event-db
+(reg-event-db
   ::set-post-metadata
   (fn [db [_ post-id metadata]]
     (assoc-in db [:posts post-id :metadata] metadata)))
 
-(re-frame/reg-event-db
+(reg-event-db
   ::set-post-loading
   (fn [db [_ post-id]]
     (let [posts-loading (:posts-loading db)]
       (assoc db :posts-loading (conj posts-loading post-id)))))
 
-(re-frame/reg-event-db
+(reg-event-db
   ::set-post-loaded
   (fn [db [_ post-id]]
     (let [posts-loading (:posts-loading db)]
